0368185c7167f8d2213f69ea489206cc24afa22e
User <User@DESKTOP-6JPNDEN.lan>
2024-01-09 14:45:59 -0500

    Main Method

---
+++ src/PR/PR.java
@@ -0,0 +1,12 @@
+package PR;
+
+public class PR {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Practice Pull Request");//Cindy
+		
+		System.out.println("Let's do this!");//Cindy
+	}
+
+}
